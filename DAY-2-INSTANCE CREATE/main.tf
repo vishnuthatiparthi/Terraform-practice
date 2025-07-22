002a0328c67e30db05b815200b7d80da8c79b550
vishnuthatiparthi <vishnureddys12345@gmail.com>
2025-07-22 21:56:40 +0530

    day3

---
+++ DAY-2-INSTANCE CREATE/main.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "name" {
+    ami = "ami-0cbbe2c6a1bb2ad63"
+    instance_type = "t2.micro"
+  
+}
---
+++ DAY-3 all config/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-0cbbe2c6a1bb2ad63"
+instance_id = "t2.micro"
